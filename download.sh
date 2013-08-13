64f214228f715998a1f7f464fe3a36069ff85c2b
yysaki <yysaki@mba>
2013-08-13 11:53:24 +0900

    add download vim colorscheme script

--- download.sh
+++ download.sh
@@ -2,19 +2,27 @@
 
 CURDIR=`dirname $0`
 
-if [ -d tmp ]; then
+if [ ! -d tmp ]; then
   mkdir tmp
 fi 
 
-echo "download japanese vimhelp..."
 cd $CURDIR/tmp
+
+echo "[echo] download and install zenburn colorscheme..."
+wget http://slinky.imukuppi.org/zenburn/zenburn.vim
+if [ ! -d ../.vim/colors ]; then
+  mkdir ../.vim/colors
+fi
+cp zenburn.vim ../.vim/colors/
+
+echo "[echo] download japanese vimhelp..."
 wget https://github.com/vim-jp/vimdoc-ja/archive/master.tar.gz
 tar zxvf master.tar.gz
 cp -rf vimdoc-ja-master/{doc,syntax} ../.vim/
 
-echo "delete tmp files..."
+echo "[echo] delete tmp files..."
 cd ..
 rm -rf $CURDIR/tmp
 
-echo "to install japanese vimhelps, command :helptags ~/dotfiles/.vim/doc/"
+echo "[echo] to install japanese vimhelps, command :helptags ~/dotfiles/.vim/doc/"
 
