c1fb47ff1720d8e3498fcef5cf349270bbb358cb
yysaki <yysaki@yysaki.com>
2014-05-03 23:53:18 +0900

    install.sh: Extract duplicated sequence from both link and unliink to link_impl

--- install.sh
+++ install.sh
@@ -32,20 +32,21 @@ unlink_dot_file () {
   rm $HOME/$1
 }
 
-link () {
-  link_dot_file $VIMFILES .vim
-  link_dot_file $VIMFILES vimfiles
+link_impl () {
+  $1 $VIMFILES .vim
+  $1 $VIMFILES vimfiles
   for DOT_FILE in $DOT_FILES; do
-    link_dot_file $DOT_FILE $DOT_FILE
+    $1 $DOT_FILE $DOT_FILE
   done
+}
+  
+
+link () {
+  link_impl link_dot_file
 }
 
 unlink () {
-  unlink_dot_file .vim
-  unlink_dot_file vimfiles
-  for DOT_FILE in $DOT_FILES; do
-    unlink_dot_file $DOT_FILE
-  done
+  link_impl unlink_dot_file
 }
 
 # git bash用, 修正した変更を集める
